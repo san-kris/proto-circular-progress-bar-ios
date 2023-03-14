be931d52572bc412f18cf7f9eeea4df77ae7abb6
Santosh Krishnamurthy <san.b350@gmail.com>
2023-03-14 10:17:59 -0500

    Add a shape later to view and draw a circle with red border

--- proto-circular-progress-bar-ios/ViewController.swift
+++ proto-circular-progress-bar-ios/ViewController.swift
@@ -11,9 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+ 
+        // use shape layer to draw a circle
+        let shapeLayer = CAShapeLayer()
+        
+        // Get the center point of the view
+        let center = view.center
+        // Draw a circular path
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: CGFloat.pi*2, clockwise: true)
+        // Add path to shape layer
+        shapeLayer.path = circularPath.cgPath
+        
+        // Add color to brder
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        // set border width
+        shapeLayer.lineWidth = 10
+        
+        view.layer.addSublayer(shapeLayer)
+        
+        
+
     }
 
+    
 
 }
 
