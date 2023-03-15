79d61987328fd6047af39e0a5b2060e91870ad4c
Santosh Krishnamurthy <san.b350@gmail.com>
2023-03-15 10:20:51 -0500

    Get notified when app moves to foreground and enable pulsating animation

--- proto-circular-progress-bar-ios/ViewController.swift
+++ proto-circular-progress-bar-ios/ViewController.swift
@@ -11,13 +11,84 @@ class ViewController: UIViewController {
 
     // use shape layer to draw a circle
     let shapeLayer = CAShapeLayer()
+    var pulsatingLayer: CAShapeLayer?
+    
+    let percentageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Start"
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    // set the font color of time and battery in status bar
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
+    
+    private func setupNotificationObserver(){
+        // Add an observer that will benootified when app returns to foreground
+        // need to declate a @objc function and provide it as #selector input
+        // Notification type = UIApplication.willEnterForegroundNotification
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(handleReturnToForeground),
+                                               name: UIApplication.willEnterForegroundNotification,
+                                               object: nil)
+        
+    }
+    
+    @objc func handleReturnToForeground(){
+        print("Foreground handler called")
+        animatePulsatingLayer()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+    }
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // minimizing the background will disable the animation
+        // we need to be nootified when application moves to foreground and add animation again
+        setupNotificationObserver()
  
+        view.backgroundColor = UIColor.black
         
         // Get the center point of the view
         let center = view.center
+
+        
+        // Setting up pulsating layer
+        pulsatingLayer = CAShapeLayer()
+
+        // set start angle to top by - .pi/2
+        let circularPulsatingPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -(CGFloat.pi / 2), endAngle: CGFloat.pi*2, clockwise: true)
+
+        // Add path to shape layer
+        pulsatingLayer?.path = circularPulsatingPath.cgPath
+        
+        // Add color to brder
+        pulsatingLayer?.strokeColor = UIColor.red.cgColor
+        // set border width
+        pulsatingLayer?.lineWidth = 10
+        
+        // clear the color
+        pulsatingLayer?.fillColor = UIColor.yellow.cgColor
+        
+        // relate position to end stroke. Range 0 - 1.0
+        // pulsatingLayer?.strokeEnd = 1.0
+        
+        //pulsatingLayer?.position = view.center
+
+        // Add track sublayer
+        view.layer.addSublayer(pulsatingLayer!)
+        
+        animatePulsatingLayer()
+        
         
         // Create a track layer
         // Draw a circular path for track later
@@ -35,11 +106,24 @@ class ViewController: UIViewController {
         // clear the color
         trackLayer.fillColor = UIColor.clear.cgColor
         
+        // Add track sublayer
+        view.layer.addSublayer(trackLayer)
+
+        
         // relate position to end stroke. Range 0 - 1.0
         shapeLayer.strokeEnd = 1.0
         
         // Add track sublayer
         view.layer.addSublayer(trackLayer)
+        
+        
+        
+        
+        
+        view.addSubview(percentageLabel)
+        percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        percentageLabel.center = view.center
+
 
 
         // Create a new run layer
@@ -69,6 +153,22 @@ class ViewController: UIViewController {
         // Add a Tap gesturetoo view
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
 
+    }
+    
+    func animatePulsatingLayer(){
+        // create an animation which changes the linewidth prooperty of the object
+        let animation = CABasicAnimation(keyPath: "lineWidth")
+        // add the to value
+        animation.toValue = 20
+        // set the duration
+        animation.duration = 1
+        // set the animation to run in infinite loop
+        animation.repeatCount = Float.infinity
+        // set the animation to reverse animate
+        animation.autoreverses = true
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        // add animatioon to a layer
+        pulsatingLayer?.add(animation, forKey: "pulsing")
     }
     
     @objc private func handleTap() -> Void {
